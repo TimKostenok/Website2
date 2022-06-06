8c6fb4946820c6c991377841bd4534281b8c0e95
TimKostenok <tim_kostenok@mail.ru>
2022-06-06 13:55:37 +0400

    initial commit

---
+++ silsite/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(redirect_field_name='')
+def base2(request):
+  
+  return render(request, 'silsite/base2.html')
+
+def login(request):
+  '''Страница входа'''
+  text = 'Sorry, but you have to log in before using site'
+  form = LoginForm(data=(request.POST or None))
+  if request.method == 'POST':
+    if form.is_valid():
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password']
+      user = authenticate(username=username, password=password)
+      if user is not None:
+        if user.is_active: # TODO: Зачем? #Нужно. # Ок
+          auth.login(request, user)
+          return redirect('/')
+        else:
+          form = LoginForm()
+          text = 'Sorry, but your account is inactive.'
+      else:
+        form = LoginForm()
+        text = 'Invalid username or password.'
+    else:
+      form = LoginForm()
+      text = 'Invalid username or password.'
+  else:
+    form = LoginForm()
+    text = 'Please, fill the form'
+  return render(request, 'silsite/login.html', {'form': form, 'text': text})
